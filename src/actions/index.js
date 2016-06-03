9b834e742815a270fff9d0dbfae98280a7ecbbc0
Yuriy Shefer <sheff@sheffmachine.com>
2016-06-03 10:56:06 -0700

    add in redux promise middleware

---
+++ src/actions/index.js
@@ -0,0 +1,2 @@
+const API_KEY = '69d29de27d0bf6197044cf01da2d4aa9';
+
--- src/containers/SearchBar.js
+++ src/containers/SearchBar.js
@@ -5,17 +5,21 @@ export default class SearchBar extends Component {
     super(props);
     this.state = { term: '' };
   }
-  onInputChange(e) {
-    console.log(e.target.value);
+  onInputChange (e) {
+    this.setState({ term: e.target.value });
+  }
+  onFormSubmit (e) {
+    e.preventDefault();
+    //go fetch the weather data
   }
   render() {
     return (
-      <form className="input-group">
+      <form onSubmit={ this.onFormSubmit.bind(this) } className="input-group">
         <input
           placeholder="Get a five day forecast in your favorite cities"
           className="form-control"
           value={ this.state.term }
-          onChange={ this.onInputChange } />
+          onChange={ this.onInputChange.bind(this) } />
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">Submit</button>
         </span>
